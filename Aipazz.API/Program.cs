7615e1cf26392d383a4f11b655cc6ae425cd7372
sajidha <sajidhahuzair19@gmail.com>
2025-04-10 12:26:18 +0530

    swagger update

--- Aipazz.API/Program.cs
+++ Aipazz.API/Program.cs
@@ -15,7 +15,7 @@ builder.Services.AddScoped<IDocumentService, DocumentService>();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
+if (app.Environment.IsDevelopment() || app.Environment.IsProduction())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
