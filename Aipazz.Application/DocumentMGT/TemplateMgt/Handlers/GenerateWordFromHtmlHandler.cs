6dd1593720f3d2a8307a7fa3bd93c5d4fa462b4b
Kappanile Kavindu Lakshan Kottegoda <138689980+kavi17498@users.noreply.github.com>
2025-04-14 08:57:54 +0530

    added git ingore to the all the layers

---
+++ Aipazz.Application/DocumentMGT/TemplateMgt/Handlers/GenerateWordFromHtmlHandler.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Aipazz.Application.DocumentMGT.Interfaces;
+using Aipazz.Application.DocumentMGT.TemplateMgt.Queries;
+using MediatR;
+
+namespace Aipazz.Application.DocumentMGT.TemplateMgt.Handlers
+{
+    public class GenerateWordFromHtmlHandler : IRequestHandler<GenerateWordFromHtmlQuery, byte[]>
+    {
+        private readonly IWordGenerator _wordGenerator;
+
+        public GenerateWordFromHtmlHandler(IWordGenerator wordGenerator)
+        {
+            _wordGenerator = wordGenerator;
+        }
+
+
+        public Task<byte[]> Handle(GenerateWordFromHtmlQuery request, CancellationToken cancellationToken)
+        {
+            var fileBytes = _wordGenerator.GenerateFromHtml(request.HtmlContent);
+            return Task.FromResult(fileBytes);
+        }
+    }
+}
