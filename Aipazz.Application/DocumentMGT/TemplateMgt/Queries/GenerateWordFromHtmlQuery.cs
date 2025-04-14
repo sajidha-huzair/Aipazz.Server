b79e338d0156a61ecfbd67d84b518d8c4145fbaa
Kappanile Kavindu Lakshan Kottegoda <138689980+kavi17498@users.noreply.github.com>
2025-04-14 09:01:11 +0530

    added git ingore to all layers

--- Aipazz.Application/DocumentMGT/TemplateMgt/Queries/GenerateWordFromHtmlQuery.cs
+++ Aipazz.Application/DocumentMGT/TemplateMgt/Queries/GenerateWordFromHtmlQuery.cs
@@ -1,18 +1,18 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using MediatR;
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Text;
+//using System.Threading.Tasks;
+//using MediatR;
 
-namespace Aipazz.Application.DocumentMGT.TemplateMgt.Queries
-{
-    public class GenerateWordFromHtmlQuery : IRequest<byte[]>
-    {
-        public string HtmlContent { get; set; }
-        public GenerateWordFromHtmlQuery(string htmlContent)
-        {
-            HtmlContent = htmlContent;
-        }
-    }
-}
+//namespace Aipazz.Application.DocumentMGT.TemplateMgt.Queries
+//{
+//    public class GenerateWordFromHtmlQuery : IRequest<byte[]>
+//    {
+//        public string HtmlContent { get; set; }
+//        public GenerateWordFromHtmlQuery(string htmlContent)
+//        {
+//            HtmlContent = htmlContent;
+//        }
+//    }
+//}
