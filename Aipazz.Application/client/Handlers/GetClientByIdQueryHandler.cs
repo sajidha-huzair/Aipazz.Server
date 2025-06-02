909d434b408e89a808d8caa7ff683c9073aef461
sajidha <sajidhahuzair19@gmail.com>
2025-06-02 09:05:15 +0530

    getclientbyid fix

---
+++ Aipazz.Application/client/Handlers/GetClientByIdQueryHandler.cs
@@ -0,0 +1,27 @@
+﻿using Aipazz.Application.client.Interfaces;
+using Aipazz.Application.client.Queries;
+using Aipazz.Domian.client;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aipazz.Application.client.Handlers
+{
+    public class GetClientByIdQueryHandler : IRequestHandler<GetClientByIdQuery, Client>
+    {
+        private readonly IClientRepository _clientRepository;
+
+        public GetClientByIdQueryHandler(IClientRepository clientRepository)
+        {
+            _clientRepository = clientRepository;
+        }
+
+        public async Task<Client> Handle(GetClientByIdQuery request, CancellationToken cancellationToken)
+        {
+            return await _clientRepository.GetByIdAsync(request.Id,request.nic);
+        }
+    }
+}
