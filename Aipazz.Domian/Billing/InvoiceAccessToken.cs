b475fe11dd0a8fcab2949b7d79f3929a99243dac
sajidha <sajidhahuzair19@gmail.com>
2025-07-18 16:38:56 +0530

    client mail and access token

---
+++ Aipazz.Domian/Billing/InvoiceAccessToken.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aipazz.Domian.Billing
+{
+    public class InvoiceAccessToken
+    {
+        public string id { get; set; }=string.Empty;
+        public string UserId { get; set; } = string.Empty;
+        public string InvoiceId { get; set; } = string.Empty;
+        public string RecipientEmail { get; set; } = string.Empty;
+        public string Token { get; set; } = string.Empty;
+        public bool OtpVerified { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public DateTime ExpiresAt { get; set; }
+    }
+
+}
