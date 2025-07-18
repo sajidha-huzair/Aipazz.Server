ea695ff46482b2095875dae11562c0af9245302d
sajidha <sajidhahuzair19@gmail.com>
2025-07-18 17:50:18 +0530

    token

---
+++ Aipazz.Application/Billing/Invoices/Commands/VerifyTokenCommand.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MediatR;
+using Aipazz.Application.Billing.DTOs;
+
+namespace Aipazz.Application.Billing.Invoices.Commands
+{
+
+        public class VerifyTokenCommand : IRequest<VerifyTokenResult>
+        {
+            public string Token { get; }
+
+            public VerifyTokenCommand(string token)
+            {
+                Token = token;
+            }
+        }
+    }
+
+
